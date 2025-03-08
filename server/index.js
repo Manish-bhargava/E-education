4ffb90a53d9e72b7aef94d9e8f1d5c5292afb53f
manish-bhargava07 <work.manish07@gmail.com>
2025-03-09 00:56:35 +0530

    changin url to frontend url

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:5173", "https://e-education.onrender.com"],
+    origin:"https://e-education-1.onrender.com/",
     credentials:true
 }));
  
